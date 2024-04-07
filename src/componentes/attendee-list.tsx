49be3da2e7e9d4de12e8446c7efd9a5b443162bc
IvambergSilva <ivambergisilva@gmail.com>
2024-04-07 20:19:59 -0300

    :sparkles: feat: Implementando a API

--- src/componentes/attendee-list.tsx
+++ src/componentes/attendee-list.tsx
@@ -3,48 +3,101 @@ import { Table } from "./table/table";
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, Ellipsis, Search } from 'lucide-react'
 import { TableHeader } from "./table/table-header";
 import TableCell from "./table/table-cell";
-import { ChangeEvent, useState } from "react";
-import { attendees } from "../data/attendees";
+import { ChangeEvent, useEffect, useState } from "react";
 import dayjs from "dayjs";
 import 'dayjs/locale/pt-br';
 import relativeTime from "dayjs/plugin/relativeTime"
 
 dayjs.extend(relativeTime);
-dayjs.locale('pt-br')
+dayjs.locale('pt-br');
+
+interface IAttendeeProps {
+    id: string;
+    name: string;
+    socialName?: string;
+    email: string;
+    createdAt: string;
+    checkedInAt: string | null;
+}
 
 export function AttendeeList() {
-    const [search, setSearch] = useState('')
+    const [totalAttendees, setTotalAttendees] = useState(0)
+    const [eventTitle, setEventTitle] = useState('')
+    const [attendees, setAttendees] = useState<IAttendeeProps[]>([])
+
+    const [search, setSearch] = useState(() => {
+        const url = new URL(window.location.toString())
+
+        if (url.searchParams.has('search')) {
+            return url.searchParams.get('search') ?? ""
+        }
+
+        return ""
+    })
+
+    const [page, setPage] = useState(() => {
+        const url = new URL(window.location.toString())
+        if (url.searchParams.has('page')) {
+            return Number(url.searchParams.get('page'))
+        }
+        return 1
+    })
 
     function handleInputSearch(event: ChangeEvent<HTMLInputElement>) {
         setSearch(event.target.value)
-    }
-
-    const [page, setPage] = useState(1)
-
-    const totalPages = attendees.length / 10
-    const lastPage = Math.ceil(totalPages)
-
-    function goToFirstPage() {
+        setCurrentSearch(event.target.value)
         setPage(1)
     }
 
-    function goToPreviousPage() {
-        if (page > 1) {
-            setPage(page - 1)
+    useEffect(() => {
+        const url = new URL('http://localhost:3333/events/bef2c7e9-feb1-49b7-a127-d0791e09bd5b/attendees');
+
+        url.searchParams.set('pageIndex', String(page - 1))
+
+        if (search.length > 1) url.searchParams.set('query', search);
+
+        fetch(url).then(response => response.json()).then((data) => {
+            setAttendees(data.attendees)
+            setTotalAttendees(data.total)
+            setEventTitle(data.eventTitle);
+        })
+
+        if (page > totalPages) {
+            setCurrentPage(1)
+        }
+
+    }, [page, search])
+
+    const totalPages = Math.ceil(totalAttendees / 10)
+
+    const setCurrentSearch = (search: string) => {
+        const url = new URL(window.location.toString());
+        url.searchParams.set('search', search);
+        window.history.pushState({}, "", url);
+        setSearch(search)
+    }
+
+    const setCurrentPage = (page: number) => {
+        if (page <= totalPages) {
+            const url = new URL(window.location.toString());
+            url.searchParams.set('page', String(page));
+            window.history.pushState({}, "", url);
+            setPage(page)
         }
     }
 
-    function goToNextPage() {
-        setPage(page + 1)
+    const goToFirstPage = () => setCurrentPage(1);
+    const goToPreviousPage = () => {
+        if (page > 1) setCurrentPage(page - 1)
     }
-
-    function goToLastPage() {
-        setPage(lastPage)
-    }
+    const goToNextPage = () => setCurrentPage(page + 1)
+    const goToLastPage = () => setCurrentPage(totalPages)
 
     return (
         <div className="flex flex-col gap-4">
-            <section className="flex gap-3 items-center">
+            <h1 className="text-3xl font-bold bg-blue-900 px-10 py-2 rounded-lg w-fit m-auto border border-white/15">{eventTitle}</h1>
+
+            <header className="flex gap-7 items-center">
                 <h1 className="text-2xl font-bold">Participantes</h1>
 
                 <div className="flex items-center w-72 gap-3 border border-white/15 px-3 py-1.5 rounded-lg">
@@ -52,42 +105,45 @@ export function AttendeeList() {
                     <input
                         type="text"
                         placeholder="Buscar participantes..."
-                        className=" text-purple-300 outline-none bg-transparent flex-1 border-none"
+                        className=" text-purple-300 outline-none bg-transparent flex-1 border-none focus:ring-0"
                         value={search}
                         onChange={handleInputSearch}
+                        autoFocus
                     />
                 </div>
-            </section>
+            </header>
 
             <Table>
-                <thead className="gap-5">
+                <thead>
                     <tr className="border-b border-white/15">
-                        <TableHeader>
+                        <TableHeader style={{ width: '10%' }}>
                             <input type="checkbox" className="size-4 bg-black/20 rounded" />
                         </TableHeader>
-                        <TableHeader>Código</TableHeader>
-                        <TableHeader>Participante</TableHeader>
+                        <TableHeader style={{ width: '10%' }}>Código</TableHeader>
+                        <TableHeader style={{ width: '30%' }}>Participante</TableHeader>
                         <TableHeader>Data da inscrição</TableHeader>
                         <TableHeader>Data do Check-in</TableHeader>
                         <TableHeader></TableHeader>
                     </tr>
                 </thead>
                 <tbody>
-                    {attendees.slice((page - 1) * 10, (page * 10)).map((attendee) => {
+                    {attendees.map((attendee) => {
                         return (
-                            <tr className="border border-white/15">
+                            <tr className="border border-white/15 hover:bg-zinc-900">
                                 <TableCell>
                                     <input type="checkbox" className="size-4 bg-black/20 rounded-sm" />
                                 </TableCell>
-                                <TableCell>{attendee.id}</TableCell>
+                                <TableCell>{attendee.id.toUpperCase()}</TableCell>
                                 <TableCell>
                                     <div className="flex flex-col gap-1">
-                                        <span className="text-purple-400 font-semibold">{attendee.name}</span>
+                                        <span className="text-purple-400 font-semibold">{attendee.socialName || attendee.name}</span>
                                         <span className="text-xs">{attendee.email}</span>
                                     </div>
                                 </TableCell>
                                 <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-                                <TableCell>{dayjs().to(attendee.checkInAt)}</TableCell>
+                                <TableCell>{attendee.checkedInAt === null
+                                    ? <span className="text-zinc-400">Não fez o check-in</span>
+                                    : dayjs().to(attendee.checkedInAt)}</TableCell>
                                 <td>
                                     <IconButton transparent>
                                         <Ellipsis className="size-4 text-purple-400" />
@@ -99,11 +155,11 @@ export function AttendeeList() {
                 </tbody>
                 <tfoot>
                     <tr>
-                        <TableCell colSpan={3}>Mostrando 10 de {attendees.length}</TableCell>
+                        <TableCell colSpan={3}>Mostrando {attendees.length} de {totalAttendees}</TableCell>
 
                         <TableCell colSpan={3}>
                             <div className="flex justify-end gap-8 items-center">
-                                <span className="text-zinc-200 text-sm">Página {page} de {lastPage}</span>
+                                <span className="text-zinc-200 text-sm">Página {page} de {attendees.length === 0 ? 1 : totalPages}</span>
 
                                 <div className="flex gap-1.5">
                                     <IconButton
@@ -121,16 +177,16 @@ export function AttendeeList() {
                                         <ChevronLeft className="size-4" />
                                     </IconButton>
                                     <IconButton
-                                        isActive={page < lastPage}
+                                        isActive={page < totalPages}
                                         onClick={goToNextPage}
-                                        disabled={page === lastPage}
+                                        disabled={page === totalPages}
                                     >
                                         <ChevronRight className="size-4" />
                                     </IconButton>
                                     <IconButton
-                                        isActive={page < lastPage}
+                                        isActive={page < totalPages}
                                         onClick={goToLastPage}
-                                        disabled={page === lastPage}
+                                        disabled={page === totalPages}
                                     >
                                         <ChevronsRight className="size-4" />
                                     </IconButton>
--- src/componentes/table/table-cell.tsx
+++ src/componentes/table/table-cell.tsx
@@ -4,6 +4,6 @@ interface ITableCellProps extends ComponentProps<'td'> { }
 
 export default function TableCell(props: ITableCellProps) {
     return (
-        <td className="py-3 px-4 text-zinc-400 text-sm" {...props} />
+        <td className="py-3 px-4 text-zinc-300 text-sm" {...props} />
     )
 }
